c83e33284caa35a81bf2b719e5a41149e056150d
Ramshankar S <13503230+ramshankarsabari@users.noreply.github.com>
2020-11-11 11:36:49 +0530

    thunk added - for asynchronous state store in reducer

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,3 +4,13 @@ import {ADD_ARTICLE} from '../constants/index';
 export function AddArticle(payload){
     return {type: ADD_ARTICLE, payload};
 }
+
+export function getData() {
+    return function(dispatch) {
+      return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => {
+          dispatch({ type: "DATA_LOADED", payload: json });
+        });
+    };
+  }
